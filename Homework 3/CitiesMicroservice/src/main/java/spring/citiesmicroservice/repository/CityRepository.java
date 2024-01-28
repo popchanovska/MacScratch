d0da6223984eca0c52f9b70f6462e2b5a9ee537a
popchanovska <e.popchanovska@gmail.com>
2024-01-28 21:29:24 +0100

    'CityServiceFixes'

--- Homework 3/CitiesMicroservice/src/main/java/spring/citiesmicroservice/repository/CityRepository.java
+++ Homework 3/CitiesMicroservice/src/main/java/spring/citiesmicroservice/repository/CityRepository.java
@@ -1,13 +1,8 @@
 package spring.citiesmicroservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import spring.citiesmicroservice.model.City;
 
-import java.util.List;
-
-@Repository
 public interface CityRepository extends JpaRepository<City, Long> {
     City findCitiesByName(String name);
-    List<City> findAll();
 }
--- Homework 3/CitiesMicroservice/src/main/java/spring/citiesmicroservice/web/Controller.java
+++ Homework 3/CitiesMicroservice/src/main/java/spring/citiesmicroservice/web/Controller.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/cities-monuments")
-
 public class Controller {
     @Autowired
     private MonumentService monumentService;
